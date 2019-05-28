2e1b6185dbcd7c206c7a3fdfff51cc41af3d682e
Adeoye Olatunbosun <adeoyeolatunbosun@Adeoyes-MacBook-Pro.local>
2019-05-28 14:51:43 +0100

    chore-[166272797]: implement babel and nodemon setup - refactor ./config/index.js file for more readability

--- config/index.js
+++ config/index.js
@@ -1,4 +1,1 @@
-export default {
-    secret:
-        process.env.NODE_ENV === "production" ? process.env.SECRET : "secret"
-};
+export const secret = process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
