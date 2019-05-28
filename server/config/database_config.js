accf085d5dccdf3f8f01ae01f13299e7132849a7
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-05-28 17:27:18 +0100

    chore-[166284571]: setup postgres
    - setupt postgres engine
    - added the .env file to gitignore
    
    [Delivers #166284571]

---
+++ server/config/database_config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE_TEST || 'kingsmen_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variables: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
+    logging: false,
+  }
+};
---
+++ server/models/index.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+const fs = require('fs');
+
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/database_config')[env];
+
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect,
+    logging: false,
+    operatorsAliases: Sequelize.Op,
+  });
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => (file.indexOf('.') !== 0)
+    && (file !== basename)
+    && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
