d30a201f872040ab96132987a197372fd5e597db
Fidelis Ojeah <fidelis_oj@yahoo.co.uk>
2019-06-11 15:09:48 +0100

    Allow module alias, set up quick test using sqlite (#22)
    * allow module alias, set up quick test using sqlite
    
    * fix missing semicolon
    
    * additional tests, remove unneeded and unused files

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,10 +1,10 @@
 import bcrypt from 'bcrypt';
-import models from '../models';
-import { validateLogin, validateSignup, updateDetails } from '../validations/auth';
-import Token from '../helpers/Token';
-import userExtractor from '../helpers/userExtractor';
-import { validationResponse, validateUniqueResponse } from '../helpers/validationResponse';
-import Response from '../helpers/Response';
+import models from '@models';
+import { validateLogin, validateSignup, updateDetails } from '@validations/auth';
+import Token from '@helpers/Token';
+import userExtractor from '@helpers/userExtractor';
+import { validationResponse, validateUniqueResponse } from '@helpers/validationResponse';
+import Response from '@helpers/Response';
 
 const { User } = models;
 
--- server/migrations/001-add-extension.js
+++ server/migrations/001-add-extension.js
@@ -1,4 +1,12 @@
-export const up = queryInterface => queryInterface
-  .sequelize
-  .query('CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;');
+const isTestEnvironment = process.env.NODE_ENV === 'test';
+
+export const up = (queryInterface) => {
+  if (isTestEnvironment) {
+    return new Promise(resolve => resolve(1));
+  }
+  return queryInterface
+    .sequelize
+    .query('CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;');
+};
+
 export const down = () => new Promise(resolve => resolve(1));
