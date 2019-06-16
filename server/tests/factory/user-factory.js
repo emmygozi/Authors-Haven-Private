d5acccdc8bb3b5c497ca33320331f1fedc1f3fb0
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-06-16 18:45:15 +0100

    bug-[166721004]: factory implementation
    - update the factory implementation
    
    [Finishes #166721004]

--- server/tests/factory/user-factory.js
+++ server/tests/factory/user-factory.js
@@ -3,22 +3,32 @@ import faker from 'faker';
 import Token from '@helpers/Token';
 import { createTestProfile } from './profile-factory';
 
-const { User } = models;
+const { User, Profile } = models;
 
 const generateToken = async (userDetails) => {
   const token = await Token.create(userDetails);
   return token;
 };
 
-const createTestUser = async ({ username, email }) => {
+const createTestUser = async ({
+  username, email, firstname, lastname, bio, avatar, location
+}) => {
   const newUser = await User.create({
     id: faker.random.uuid(),
     username: username || faker.internet.userName(),
     email: email || faker.internet.email(),
     password: faker.internet.password()
   });
+  console.log(newUser);
+  await Profile.create({
+    userId: newUser.dataValues.id,
+    firstname: firstname || faker.name.firstName(),
+    lastname: lastname || faker.name.lastName(),
+    bio: bio || faker.lorem.sentence(),
+    avatar: avatar || faker.image.imageUrl(),
+    location: location || faker.address.country()
+  });
 
-  await createTestProfile(newUser);
   return newUser;
 };
 
@@ -29,7 +39,7 @@ const createTestUserWithoutProfile = async ({ username, email }) => {
     email: email || faker.internet.email(),
     password: faker.internet.password()
   });
-
+  await createTestProfile(newUser);
   return newUser;
 };
 
